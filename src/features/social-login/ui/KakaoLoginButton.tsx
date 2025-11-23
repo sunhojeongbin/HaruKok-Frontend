fb009b5199798b64826d107ecad4e9d2caf542bb
최정빈 <c.je0ngb2n@gmail.com>
2025-11-23 13:31:50 +0900

    feat(features/social-login): 카카오 로그인 버튼 컴포넌트 추가

---
+++ src/features/social-login/ui/KakaoLoginButton.tsx
@@ -0,0 +1,8 @@
+export const KakaoLoginButton = () => {
+    return (
+        <button className='flex items-center justify-center w-80 h-12 rounded-lg bg-[#fee500] text-[rgb(0,0,0,85)] font-[500]'>
+            <img src='/icons/ic-kakao-symbol.svg' alt='카카오' className='w-5 h-5 mr-2' />
+            카카오 로그인
+        </button>
+    );
+};
