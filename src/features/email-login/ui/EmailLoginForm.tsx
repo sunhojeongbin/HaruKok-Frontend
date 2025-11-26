f1583ea98349e7c0912a409a05e1dbe2347f1310
최정빈 <c.je0ngb2n@gmail.com>
2025-11-26 23:45:34 +0900

    feat(features/email-login): 이메일 로그인 폼 컴포넌트 추가

---
+++ src/features/email-login/ui/EmailLoginForm.tsx
@@ -0,0 +1,19 @@
+import { Button, Input, Label } from '../../../shared/ui';
+
+export const EmailLoginForm = () => {
+    return (
+        <form className='flex flex-col gap-8'>
+            <div className='flex flex-col gap-4'>
+                <div>
+                    <Label htmlFor='email'>이메일</Label>
+                    <Input type='email' id='email' placeholder='이메일을 입력해 주세요.' />
+                </div>
+                <div>
+                    <Label htmlFor='password'>비밀번호</Label>
+                    <Input type='password' id='password' placeholder='비밀번호를 입력해 주세요.' />
+                </div>
+            </div>
+            <Button onClick={() => {}}>로그인</Button>
+        </form>
+    );
+};
