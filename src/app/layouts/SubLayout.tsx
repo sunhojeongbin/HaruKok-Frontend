c8d154fa23c0bf5061a03df4f666140e4cb86fbe
최정빈 <jbchoi3456@gmail.com>
2026-01-08 17:18:10 +0900

    style(app/layouts): 서브 레이아웃 스타일 수정

--- src/app/layouts/SubLayout.tsx
+++ src/app/layouts/SubLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom';
 export const SubLayout = () => {
   return (
     <>
-      <main className='pt-12'>
+      <main className='pt-16'>
         <Outlet />
       </main>
     </>
