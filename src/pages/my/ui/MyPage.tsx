8c996613a0a1c0774a71984724014a7e5e2807e9
최정빈 <jbchoi3456@gmail.com>
2026-01-08 15:16:35 +0900

    chore: My를 Profile로 변경

--- src/pages/my/ui/MyPage.tsx
+++
@@ -1,3 +0,0 @@
-export const MyPage = () => {
-  return <div>My Page</div>;
-};
--- src/shared/assets/icons/index.ts
+++ src/shared/assets/icons/index.ts
@@ -2,9 +2,9 @@ import ArrowLeft from './arrow-left.svg?react';
 import Calendar from './calendar.svg?react';
 import Home from './home.svg?react';
 import Management from './management.svg?react';
-import My from './my.svg?react';
 import Notification from './notification.svg?react';
+import Profile from './profile.svg?react';
 import Search from './search.svg?react';
 import Settings from './settings.svg?react';
 
-export { ArrowLeft, Calendar, Home, Management, My, Notification, Search, Settings };
+export { ArrowLeft, Calendar, Home, Management, Notification, Profile, Search, Settings };
