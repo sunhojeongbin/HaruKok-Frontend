ae2480636ce900ce21fbff15281772cc436135c5
최정빈 <jbchoi3456@gmail.com>
2026-01-26 01:54:58 +0900

    feat(pages/todo): 투두 페이지 추가

--- src/pages/todo/ui/TodoPage.tsx
+++ src/pages/todo/ui/TodoPage.tsx
@@ -1,9 +1,11 @@
 import { Calendar } from '../../../shared/ui/Calendar';
+import { TodoList } from '../../../widgets/todo-list/ui/TodoList';
 
 export const TodoPage = () => {
   return (
-    <div className='min-h-screen bg-[#f3f4f6]'>
+    <div className='flex min-h-full flex-col gap-4 bg-[#f3f4f6] px-4 pb-4'>
       <Calendar />
+      <TodoList />
     </div>
   );
 };
