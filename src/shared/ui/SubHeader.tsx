b6c971af2dde9a3dffcb56da00ec0dd31ad99553
최정빈 <jbchoi3456@gmail.com>
2026-02-13 15:09:35 +0900

    style(shared/ui): 서브 헤더 스타일 수정

--- src/shared/ui/SubHeader.tsx
+++ src/shared/ui/SubHeader.tsx
@@ -1,17 +1,26 @@
+import { useNavigate } from 'react-router-dom';
+
 import { IconButton } from './IconButton';
 
-interface IHeaderProps {
+interface ISubHeaderProps {
   children: React.ReactNode;
+  onPlus?: () => void;
 }
 
-export const SubHeader = ({ children }: IHeaderProps) => {
+export const SubHeader = ({ children, onPlus }: ISubHeaderProps) => {
+  const navigate = useNavigate();
+
   return (
-    <header className='w-full h-12 grid mb-4 grid-cols-3 items-center'>
+    <header className='grid h-14 shrink-0 grid-cols-3 items-center px-4'>
       <div className='flex items-center'>
-        <IconButton icon='ArrowLeft' onClick={() => window.history.back()} />
+        <IconButton icon='ArrowLeft' onClick={() => navigate(-1)} />
       </div>
 
-      <div className='text-[#313131] text-lg font-[500] text-center'>{children}</div>
+      <div className='text-center font-semibold'>{children}</div>
+
+      <div className='flex justify-end'>
+        {onPlus && <IconButton icon='Plus' onClick={onPlus} />}
+      </div>
     </header>
   );
 };
