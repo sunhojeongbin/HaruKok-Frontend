4f034a979657148e1e6dda6c7eac5fc49826a777
최정빈 <jbchoi3456@gmail.com>
2026-05-29 11:30:36 +0900

    chore: 경로 별칭 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+import { fileURLToPath } from 'node:url';
+
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
@@ -9,6 +11,17 @@ export default defineConfig(({ mode }) => {
 
   return {
     plugins: [react(), tailwindcss(), svgr()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        '@app': fileURLToPath(new URL('./src/app', import.meta.url)),
+        '@pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
+        '@widgets': fileURLToPath(new URL('./src/widgets', import.meta.url)),
+        '@features': fileURLToPath(new URL('./src/features', import.meta.url)),
+        '@entities': fileURLToPath(new URL('./src/entities', import.meta.url)),
+        '@shared': fileURLToPath(new URL('./src/shared', import.meta.url)),
+      },
+    },
     server: {
       proxy: {
         '/api': {
