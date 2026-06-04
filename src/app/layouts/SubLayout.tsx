06973dfa5b1ebfbbf2242c59f16ffd89510493be
최정빈 <jbchoi3456@gmail.com>
2026-06-04 15:52:02 +0900

    style(app/layouts): 서브 레이아웃 스타일 수정

--- src/app/layouts/SubLayout.tsx
+++ src/app/layouts/SubLayout.tsx
@@ -1,16 +1,23 @@
-import { SubHeader } from '../../shared/ui';
+import { SubHeader } from '@shared/ui';
 
-interface ISubLayoutProps {
+interface SubLayoutProps {
   title: string;
   children: React.ReactNode;
+  disabled?: boolean;
+  onPlus?: () => void;
+  onSubmit?: () => void;
 }
 
 // TODO: iOS 웹뷰에서 키보드가 올라올 때 레이아웃 깨짐 이슈 해결
-export const SubLayout = ({ title, children }: ISubLayoutProps) => {
+export const SubLayout = ({ title, children, disabled, onPlus, onSubmit }: SubLayoutProps) => {
   return (
     <div className='flex h-screen flex-col'>
-      <SubHeader>{title}</SubHeader>
-      <main className='flex-1 overflow-y-auto overscroll-none px-4 pt-4'>{children}</main>
+      <SubHeader disabled={disabled} onPlus={onPlus} onSubmit={onSubmit}>
+        {title}
+      </SubHeader>
+      <main className='flex min-h-0 flex-1 flex-col overflow-y-auto overscroll-none p-4'>
+        {children}
+      </main>
     </div>
   );
 };
